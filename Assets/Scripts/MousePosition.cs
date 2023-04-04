c306c5cb430d491db621c0e7bdf15c50be3f76b5
rswansonbradley <raswanson@mail.bradley.edu>
2023-04-03 23:24:55 -0500

    StartOfPowers

--- Assets/Scripts/MousePosition.cs
+++ Assets/Scripts/MousePosition.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MousePosition : MonoBehaviour
 {
     public Camera mainCam;
+    public LayerMask PlanetLayer;
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/PowerSelected.cs
+++ Assets/Scripts/PowerSelected.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class PowerSelected : MonoBehaviour
 {
-    enum powers
+    public enum powers
     {
         Rain,
         Smite,
         Growth,
         Love
     };
-    powers currentpower;
+    public powers currentpower;
+
+    public GameObject RainPower;
+    public GameObject SmitePower;
+    public GameObject GrowthPower;
+    public GameObject LovePower;
+
+    public MousePosition MousePos;
+
+    public GameObject PlanetCenter;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +31,22 @@ public class PowerSelected : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Power Used");
+            GameObject createdPower;
+            if(currentpower == powers.Rain)
+            {
+                createdPower = Instantiate(RainPower, MousePos.transform.position, Quaternion.identity);
+            }
+            else
+            {
+                createdPower = null;
+            }
+            createdPower.transform.LookAt(PlanetCenter.transform.position);
+            createdPower.transform.Rotate(new Vector3(createdPower.transform.rotation.x +90, 
+                createdPower.transform.rotation.y + 90, createdPower.transform.rotation.z));
+        }
     }
 
     public void SelectRain()
