6972e143553ba14376f725806e5e3a1d939cccb5
rswansonbradley <raswanson@mail.bradley.edu>
2023-04-05 19:15:27 -0500

    HumanAnimalTracker

--- Assets/Scripts/PlanetTargets.cs
+++ Assets/Scripts/PlanetTargets.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class PlanetTargets : MonoBehaviour
 {
     public GameObject[] targets = new GameObject[1482];
+    //public List<GameObject> targets1;
     // Start is called before the first frame update
     void Start()
     {
         targets = GameObject.FindGameObjectsWithTag("PlanetMoment");
+        /*foreach(GameObject t in GameObject.FindGameObjectsWithTag("PlanetMoment"))
+        {
+            targets1.Add(t);
+        }*/
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         targets = GameObject.FindGameObjectsWithTag("PlanetMoment");
+        //targets1.RemoveAll(x => x == null);
     }
 }
